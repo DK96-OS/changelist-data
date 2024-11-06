fa0ab93573352b0b677b3fe9d490dc6e65f4a1ae
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-11-06 12:52:52 -0500

    Test: * Update test/changelist_data/xml/workspace/test_workspace_reader.py

--- test/changelist_data/xml/workspace/test_workspace_reader.py
+++ test/changelist_data/xml/workspace/test_workspace_reader.py
@@ -5,8 +5,13 @@ from changelist_data.xml.workspace.workspace_reader import find_changelist_manag
 from test.changelist_data.xml.workspace.provider import get_no_changelist_xml, get_simple_changelist_xml, get_multi_changelist_xml
 
 
-def test_find_changelist_manager_empty_xml_returns_none():
-    assert find_changelist_manager(parse_xml("")) is None
+def test_find_changelist_manager_empty_xml_raises_exit():
+    try:
+        find_changelist_manager(parse_xml(""))
+        raises_exit = False
+    except SystemExit:
+        raises_exit = True
+    assert raises_exit
 
 
 def test_find_changelist_manager_no_changelist_returns_none():
