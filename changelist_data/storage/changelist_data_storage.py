7243e71bb03430a7734ad7f3a5847b8677937d66
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-11-14 11:34:10 -0500

    Changelist Data: * Update changelist_data/xml/base_xml_tree.py - make abstract methods raise NotImplemented, debug method write_tree * Update changelist_data/storage/changelist_data_storage.py - add docs, modify method write_to_storage to return result of base_xml_tree write_tree method

--- changelist_data/storage/changelist_data_storage.py
+++ changelist_data/storage/changelist_data_storage.py
@@ -10,7 +10,12 @@ from changelist_data.xml.base_xml_tree import BaseXMLTree
 
 @dataclass(frozen=True)
 class ChangelistDataStorage:
-    """
+    """ Controller Interface for Data Storage.
+
+    Fields:
+    - base_xml_tree (BaseXMLTree): A Wrapper around XML ElementTree, compatible with both xml format Storage Types.
+    - storage_type (StorageType): An enum selecting the specific xml format used.
+    - update_path (Path): The file Path where Data is read from and written to.
     """
     base_xml_tree: BaseXMLTree
     storage_type: StorageType
@@ -19,9 +24,21 @@ class ChangelistDataStorage:
     def get_changelists(self) -> list[Changelist]:
         return self.base_xml_tree.get_changelists()
 
-    def update_changelists(self, changelists: list[Changelist]):
+    def update_changelists(
+        self, changelists: list[Changelist]
+    ):
+        """ Overwrite the collection of Changelist data in Memory.
+
+        Parameters:
+        - changelists (list[Changelist]): The new list of Changelists.
+        """
         self.base_xml_tree.update_changelists(changelists)
 
     def write_to_storage(self) -> bool:
-        self.base_xml_tree.write_tree(self.update_path)
-        return True
+        """ Create or overwrite storage file.
+            Ensures parent directories exist.
+
+        Returns:
+        bool - True if data was written.
+        """
+        return self.base_xml_tree.write_tree(self.update_path)
--- changelist_data/xml/base_xml_tree.py
+++ changelist_data/xml/base_xml_tree.py
@@ -2,7 +2,7 @@
 """
 from abc import ABCMeta, abstractmethod
 from pathlib import Path
-from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import ElementTree, tostring
 
 from changelist_data.changelist import Changelist
 
@@ -13,25 +13,36 @@ class BaseXMLTree(metaclass=ABCMeta):
 
     @abstractmethod
     def get_root(self) -> ElementTree:
-        pass
+        raise NotImplemented
 
     @abstractmethod
     def get_changelists(self) -> list[Changelist]:
-        pass
+        raise NotImplemented
 
     @abstractmethod
     def update_changelists(self, changelists: list[Changelist]):
-        pass
+        raise NotImplemented
 
     def write_tree(
         self, path: Path,
-    ):
+    ) -> bool:
+        """ Write the Tree as XML to the given Path.
+            Ensures that all parent directories exist, and creates the file if necessary.
+
+        Parameters:
+        - path (Path): The Path to the File.
+
+        Returns:
+        bool - True if data was written to the file.
         """
-        Write the Tree as XML to the given Path.
-        """
-        self.get_root().write(
-            file_or_filename=path,
-            encoding='utf-8',
-            xml_declaration=True,
-            method='xml',
-        )
+        if not path.exists():
+            path.mkdir(parents=True, exist_ok=True)
+            path.touch()
+        return path.write_bytes(
+            tostring(
+                element=self.get_root().getroot(),
+                encoding='utf-8',
+                method='xml',
+                xml_declaration=True,
+            )
+        ) > 0
