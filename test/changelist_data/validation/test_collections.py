4c92fe65da9b9a3df2f0fdbd1ad2059b6b614c57
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2024-12-28 20:19:16 -0500

    Test Validation Package: * Update test_collections.py - add test cases

--- test/changelist_data/validation/test_collections.py
+++ test/changelist_data/validation/test_collections.py
@@ -1,6 +1,9 @@
 """ Testing Collections.
 """
-from changelist_data.validation.collections import is_collection, divide_collection
+import pytest
+
+from changelist_data.validation.collections import is_collection, divide_collection, \
+    execute_boolean_operation_concurrently
 
 
 def test_is_collection_list_returns_true():
@@ -26,6 +29,13 @@ def test_is_collection_str_returns_false():
 def test_divide_collection_empty_returns_empty():
     result = list(divide_collection([]))
     assert len(result) == 0
+
+
+def test_divide_collection_single_char_2_num_groups_3_returns_2():
+    result = list(divide_collection(['a', 'b'], 3))
+    assert len(result) == 2
+    assert result[0] == ['a']
+    assert result[1] == ['b']
 
 
 def test_divide_collection_single_char_3_returns_3():
@@ -82,3 +92,54 @@ def test_divide_collection_single_char_7_groups_3_returns_3_groups():
     assert result[0] == ['a', 'b']
     assert result[1] == ['c', 'd']
     assert result[2] == ['e', 'f', 'g']
+
+
+@pytest.mark.parametrize("input_collection", [
+    [],
+    ["7",],
+    ["7", "65","323", "2624"],
+    ["12","32","43","12","43","65"],
+])
+def test_execute_boolean_operation_concurrently_element_length_over_4_returns_false(input_collection):
+    assert not execute_boolean_operation_concurrently(
+        input_collection, lambda x: len(x) > 4, 3
+    )
+
+@pytest.mark.parametrize("input_collection", [
+    ["svfsdvsd",],
+    ["we","adf","egr","ergsgb","dd","egv"],
+    ("we","adf","egr","ergsgb","dd","egv"),
+    ["54","12","32","43","12734","43234","65"],
+    ("54","12","32","43","12734","43234","65"),
+])
+def test_execute_boolean_operation_concurrently_element_length_over_4_returns_true(input_collection):
+    print(input_collection)
+    print(len(list(filter(lambda l: len(l) > 4, input_collection))))
+    assert execute_boolean_operation_concurrently(
+        input_collection, lambda x: 0 < len(list(filter(lambda l: len(l) > 4, x))), 3
+    )
+
+
+@pytest.mark.parametrize("input_collection", [
+    [],
+    [742,],
+    [421542, 246345,],
+    [345, 256, 356845, 425, 256, 4587, 567, 478, 3567, 4452, 24765, ],
+])
+def test_execute_boolean_operation_concurrently_element_is_negative_returns_false(input_collection):
+    assert not execute_boolean_operation_concurrently(
+        input_collection, lambda x: len(list(filter(lambda l: l < 0, x))) > 0, 3
+    )
+
+
+@pytest.mark.parametrize("input_collection", [
+    [-1,],
+    [742, -1],
+    [421542, -246345,],
+    [421542, 246345, -1],
+    [345, 256, 356845, 425, 256, 4587, 567, -1, 478, 3567, 4452, 24765, ],
+])
+def test_execute_boolean_operation_concurrently_element_is_negative_returns_true(input_collection):
+    assert execute_boolean_operation_concurrently(
+        input_collection, lambda x: len(list(filter(lambda l: l < 0, x))) > 0, 3
+    )
