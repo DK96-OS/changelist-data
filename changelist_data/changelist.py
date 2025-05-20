ea5c5cf5899e222a7380a82c01a0ab4e781c683f
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-05-19 22:14:05 -0400

    CL-Data Main Package:
    * Update changelist.py - add method compute_key

--- changelist_data/changelist.py
+++ changelist_data/changelist.py
@@ -1,6 +1,6 @@
 """The Data Class for a ChangeList.
 """
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Iterable
 
 from changelist_data.file_change import FileChange
@@ -13,13 +13,13 @@ class Changelist:
     Properties:
     - id (str): The unique id of the changelist.
     - name (str): The name of the changelist.
-    - changes (list[ChangeData]): The list of file changes in the changelist.
+    - changes (list[FileChange]): The list of FileChange data objects. Default: Empty List.
     - comment (str): The comment associated with the changelist. Default: Empty String.
     - is_default (bool): Whether this is the active changelist. Default: False.
     """
     id: str
     name: str
-    changes: list[FileChange]
+    changes: list[FileChange] = field(default_factory=lambda: [])
     comment: str = ""
     is_default: bool = False
 
@@ -36,3 +36,20 @@ def get_default_cl(
     if len(lists) > 0: # First if no default attribute found
         return lists[0]
     return None
+
+
+def compute_key(cl_name: str) -> str:
+    """ Compute a Key to use for a given Changelist Name.
+     - computation is a sequence of reduction operations
+
+    Parameters:
+    - cl_name (str): The Changelist Name to use in key computation.
+
+    Returns:
+    str - A Key that can be used for changelist lookups, or an empty str.
+    """
+    if len(cl_name) == 0:
+        return ''
+    translator = str.maketrans('', '', ' :/\\')
+    words = cl_name.translate(translator).split()
+    return ''.join(w.lower() for w in words)
