48b6e4d1d2d9f25a60fcb32e7595478b0f6e1aa8
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-20 10:15:34 -0400

    Test Main Package: * Update test_file_change - add test cases

--- test/changelist_data/test_file_change.py
+++ test/changelist_data/test_file_change.py
@@ -1,5 +1,6 @@
 """Testing Module Methods.
 """
+from changelist_data import file_change
 from test.changelist_data.conftest import MODULE_SRC_PATH
 
 
@@ -25,3 +26,29 @@ def test_properties_module_src_all(fc_all):
     assert data.before_path == MODULE_SRC_PATH
     assert not data.after_dir
     assert data.after_path == MODULE_SRC_PATH
+
+
+def test_create_fc_empty_str_returns_fc_empty_after_path():
+    result = file_change.create_fc('')
+    assert result.after_path == ''
+    assert not result.after_dir
+    # The other properties are None
+    assert result.before_path is None
+    assert result.before_dir is None
+
+
+def test_update_fc_empty_str_returns_fc_empty_str_paths():
+    result = file_change.update_fc('')
+    assert result.after_path == ''
+    assert not result.after_dir
+    assert result.before_path == ''
+    assert not result.before_dir
+
+
+def test_delete_fc_empty_str_returns_fc_empty_str_paths():
+    result = file_change.delete_fc('')
+    assert result.before_path == ''
+    assert not result.before_dir
+    # The other properties are None
+    assert result.after_path is None
+    assert result.after_dir is None
