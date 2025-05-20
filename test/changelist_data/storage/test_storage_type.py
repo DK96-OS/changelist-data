8cb2886bb9ccbc6c9822bd687b87acd2742ce05a
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-20 11:25:57 -0400

    Test Storage Package: * Update test_storage_type - add test case

--- test/changelist_data/storage/test_storage_type.py
+++ test/changelist_data/storage/test_storage_type.py
@@ -1,8 +1,9 @@
 """ Testing Storage of Changelists XML Files.
 """
+import pytest
 from pathlib import Path
 
-from changelist_data.storage import StorageType
+from changelist_data.storage import StorageType, storage_type
 from changelist_data.storage.storage_type import CHANGELISTS_FILE_PATH_STR, get_default_path, WORKSPACE_FILE_PATH_STR
 
 
@@ -12,3 +13,8 @@ def test_get_default_path_changelist():
 
 def test_get_default_path_workspace():
     assert Path(WORKSPACE_FILE_PATH_STR) == get_default_path(StorageType.WORKSPACE)
+
+
+def test_get_default_file_invalid_arg_raises_value_error():
+    with pytest.raises(ValueError, match="Invalid Argument: "):
+        storage_type.get_default_file('hellow')
