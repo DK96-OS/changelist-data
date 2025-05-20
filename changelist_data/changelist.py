f9b772c5195d338c87b6c519273240a5938f52d0
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-05-19 21:13:27 -0400

    CL-Data Main Package:
    * Update changelist.py - add method get_default_cl

--- changelist_data/changelist.py
+++ changelist_data/changelist.py
@@ -1,24 +1,38 @@
 """The Data Class for a ChangeList.
 """
 from dataclasses import dataclass
+from typing import Iterable
 
 from changelist_data.file_change import FileChange
 
 
 @dataclass(frozen=True)
 class Changelist:
-    """
-    The Data class representing a ChangeList.
+    """ The Data class representing a ChangeList.
     
     Properties:
     - id (str): The unique id of the changelist.
     - name (str): The name of the changelist.
     - changes (list[ChangeData]): The list of file changes in the changelist.
-    - comment (str): The comment associated with the changelist.
-    - is_default (bool): Whether this is the active changelist.
+    - comment (str): The comment associated with the changelist. Default: Empty String.
+    - is_default (bool): Whether this is the active changelist. Default: False.
     """
     id: str
     name: str
     changes: list[FileChange]
     comment: str = ""
     is_default: bool = False
+
+
+def get_default_cl(
+    changelists: Iterable[Changelist],
+) -> Changelist | None:
+    """ Find the Default Changelist, or set the first Changelist to default.
+        Returns None if lists is empty.
+    """
+    for cl in lists:
+        if cl.is_default:
+            return cl
+    if len(lists) > 0: # First if no default attribute found
+        return lists[0]
+    return None
