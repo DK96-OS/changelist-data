a1b6fd8f6c1ad1c8f66491355b666bf582bb6600
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-20 20:06:46 -0400

    CL-Data Workspace XML Package: * Update __init__.py - update docs, add method generate_changelists_from_xml * Update workspace_reader.py - update docs, add method generate_changelists * Update workspace_tree.py - update docs, add method generate_changelists

--- changelist_data/xml/workspace/__init__.py
+++ changelist_data/xml/workspace/__init__.py
@@ -1,5 +1,7 @@
 """ Changelist Data Storage XML Workspace
 """
+from typing import Generator
+
 from changelist_data.changelist import Changelist
 from changelist_data.xml.workspace import workspace_reader
 from changelist_data.xml.workspace.workspace_tree import WorkspaceTree
@@ -8,31 +10,38 @@ from changelist_data.xml.workspace.workspace_tree import WorkspaceTree
 def read_xml(
     workspace_xml: str
 ) -> list[Changelist]:
-    """
-    Parse the Workspace XML file and obtain all ChangeList Data in a list.
+    """ Parse the Workspace XML file and obtain all ChangeList Data in a list.
 
     Parameters:
-    - workspace_xml (str): The contents of the Workspace file, in xml format.
+     - workspace_xml (str): The contents of the Workspace file, in xml format.
     
     Returns:
-    list[Changelist] - The list of Changelists in the workspace file.
+     list[Changelist] - The list of Changelists in the workspace file.
     """
-    if (cl_manager := workspace_reader.find_changelist_manager(workspace_reader.parse_xml(workspace_xml))) is None:
-        exit("ChangeList Manager was not found in the workspace file.")
-    return workspace_reader.extract_list_elements(cl_manager)
+    return list(generate_changelists_from_xml(workspace_xml))
+
+
+def generate_changelists_from_xml(
+    workspace_xml: str
+) -> Generator[Changelist, None, None]:
+    if (cl_manager := workspace_reader.find_changelist_manager(
+        workspace_reader.parse_xml(workspace_xml)
+    )) is None:
+        exit("Changelist Manager was not found in the workspace file.")
+    yield from workspace_reader.generate_changelists(cl_manager)
+
 
 
 def load_xml(
     workspace_xml: str
 ) -> WorkspaceTree:
-    """
-    Parse the Workspace XML file into an XML Tree, and Wrap it.
+    """ Parse the Workspace XML file into an XML Tree, and Wrap it.
 
     Parameters:
-    - workspace_xml (str): The contents of the Workspace file, in xml format.
+     - workspace_xml (str): The contents of the Workspace file, in xml format.
 
     Returns:
-    WorkspaceTree - An XML Tree changelists interface.
+     WorkspaceTree - An XML Tree changelists interface.
     """
     return WorkspaceTree(
         workspace_reader.parse_xml(workspace_xml)
